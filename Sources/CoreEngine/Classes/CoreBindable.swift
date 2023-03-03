6c65edeb2df38befd02a927c8f6609ce1ea63d8b
stareta1202 <stareta1202@gmail.com>
2023-03-03 13:51:03 +0900

    Core Bindable 추가

---
+++ Sources/CoreEngine/Classes/CoreBindable.swift
@@ -0,0 +1,29 @@
+import Foundation
+import Combine
+
+protocol CoreBindable: AnyObject {
+    associatedtype Core: CoreEngine.Core
+    var core: Core? { get set }
+    var subscription: Set<AnyCancellable> { get set }
+    func bind(core: Core)
+}
+
+extension CoreBindable {
+    public var core: Core? {
+        get { return objc_getAssociatedObject(self.self, &AssociatedKeys.core) as? Core }
+        set(newCore) {
+            if let core = newCore {
+                objc_setAssociatedObject(self.self, &AssociatedKeys.core, core, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+                self.subscription.forEach({ $0.cancel() })
+                self.subscription = .init()
+                bind(core: core)
+            }
+            
+        }
+    }
+}
+
+
+internal struct AssociatedKeys {
+    static var core = "Core"
+}
