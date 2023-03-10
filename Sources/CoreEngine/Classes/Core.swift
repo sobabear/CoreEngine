b2e08ee68e498036237b4e648e94a130e01dd66c
stareta1202 <stareta1202@gmail.com>
2023-03-10 18:14:38 +0900

    WIP

--- Sources/CoreEngine/Classes/Core.swift
+++ Sources/CoreEngine/Classes/Core.swift
@@ -4,17 +4,33 @@ import Combine
 public protocol Core: ObservableObject {
     associatedtype Action
     associatedtype State
+    associatedtype Error: Swift.Error
+    associatedtype Effect: Publisher
     
+    var subscription: Set<AnyCancellable> { get set }
     var action: ((Action) -> ()) { get }
     var state: State { get set }
-    func mutate(state: State, action: Action) -> State
+    func mutate(state: State, action: Action) -> (State, Effect?)
+    
 }
 
 extension Core {
     public var action: ((Action) -> ()) {
         let _newActionClosure: ((Action) -> ()) = { [weak self] _action in
             if let self = self {
-                self.state = self.mutate(state: self.state, action: _action)
+                let (newState, effect) = self.mutate(state: self.state, action: _action)
+                self.state = newState
+                if let effect = effect {
+                    effect
+                        .sink(receiveCompletion: { completion in
+                            if case let .failure(error) = completion {
+                                // handle error
+                            }
+                        }, receiveValue: { value in
+                            // handle value
+                        })
+                        .store(in: &self.subscription)
+                }
             }
         }
         return _newActionClosure
