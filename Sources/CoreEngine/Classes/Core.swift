d673f2a55064b564f14e28e9d7a0abac269e27c0
stareta1202 <stareta1202@gmail.com>
2023-03-10 18:52:41 +0900

    WIP Error

--- Sources/CoreEngine/Classes/Core.swift
+++ Sources/CoreEngine/Classes/Core.swift
@@ -4,14 +4,14 @@ import Combine
 public protocol Core: ObservableObject {
     associatedtype Action
     associatedtype State
-    associatedtype Error: Swift.Error
     associatedtype Effect: Publisher
     
     var subscription: Set<AnyCancellable> { get set }
     var action: ((Action) -> ()) { get }
     var state: State { get set }
     func mutate(state: State, action: Action) -> (State, Effect?)
-    
+    func effect(output: Effect.Output)
+    func handleError(error: Error)
 }
 
 extension Core {
@@ -19,20 +19,23 @@ extension Core {
         let _newActionClosure: ((Action) -> ()) = { [weak self] _action in
             if let self = self {
                 let (newState, effect) = self.mutate(state: self.state, action: _action)
-                self.state = newState
                 if let effect = effect {
                     effect
-                        .sink(receiveCompletion: { completion in
+                        .sink(receiveCompletion: { [weak self] completion in
                             if case let .failure(error) = completion {
-                                // handle error
+                                self?.handleError(error: error)
                             }
-                        }, receiveValue: { value in
-                            // handle value
+                        }, receiveValue: { [weak self] value in
+                            self?.effect(output: value)
                         })
                         .store(in: &self.subscription)
                 }
+                self.state = newState
             }
         }
         return _newActionClosure
     }
+    
+    func handleError(error: Error) { }
+    func effect(output: Effect.Output) { }
 }
